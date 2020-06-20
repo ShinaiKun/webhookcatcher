7796a37a912950851e9fc872b2cb3094355edb18
Shay Kadosh <shinai@Shays-Mac.local>
2020-06-20 04:24:33 +0300

    V1.01 - catchLive now displays webhooks in live on page, with properly formatted JSON. What a life :)

--- catchLive.js
+++ catchLive.js
@@ -1,21 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const server = require('http').Server(app);
-const port = 3000
-
-const io = require('socket.io')(server);
-
+const port = 3000;
 var bodyParser = require('body-parser');
+const io = require('socket.io')(server);
+var fs = require('fs');
+const path = require('path')
 
-// app.use(express.static(__dirname + '/public'));
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/catch', (req,res) => {
-    io.on('connection', (socket) => {
-        console.log("Client connected!");
-        socket.emit('webhook', req.body);
-      });
-    res.send('Received.');
+app.get('/catch', (req, res) =>  {
+  res.sendFile(path.join(__dirname + '/client/index.html'));
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + "/public/", {
+  index: false, 
+  immutable: true, 
+  cacheControl: true,
+  maxAge: "30d"
+}));
+
+app.post('/catch', (req,res) => {
+    io.emit('webhook', req.body);
+    console.log(req.body);
+    res.send(req.body);
+})
+
+server.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
